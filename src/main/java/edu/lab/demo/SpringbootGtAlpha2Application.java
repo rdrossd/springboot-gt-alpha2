227c06ef092243f2c6472dffa547d0c2758f7e02
rdros_000 <rdros_000@education_pc.mshome.net>
2020-12-01 22:41:39 -0500

    Initial Release

---
+++ src/main/java/edu/lab/demo/SpringbootGtAlpha2Application.java
@@ -0,0 +1,13 @@
+package edu.lab.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGtAlpha2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGtAlpha2Application.class, args);
+	}
+
+}
